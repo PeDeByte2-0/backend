5c50e8dde0372239bd6902a6505407339f00a4f4
xLeotte <victorleottegaz@gmail.com>
2024-12-04 21:53:40 -0300

    login username
    mudado login para username

--- src/1services/authService.js
+++ src/1services/authService.js
@@ -2,37 +2,37 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { client } = require('../0config/database'); // Importar o cliente configurado
 
-const JWT_SECRET = 'sua-chave-secreta-aqui'; // Use variáveis de ambiente em produção
+const JWT_SECRET = process.env.JWT_SECRET; // Use variáveis de ambiente em produção
 
 module.exports = {
-  async authenticate(email, password) {
+  async authenticate(username, password) {
     try {
-      // Buscar o login no banco pelo email
-      const loginQuery = 'SELECT * FROM "PeDeByteSchema".tb_login WHERE email = $1;';
-      const loginResult = await client.query(loginQuery, [email]);
+      // Buscar o usuário na tabela tb_user pelo username
+      const userQuery = 'SELECT * FROM "PeDeByteSchema".tb_user WHERE username = $1;';
+      const userResult = await client.query(userQuery, [username]);
+
+      if (userResult.rows.length === 0) {
+        throw new Error('Usuário ou senha inválidos');
+      }
+
+      const userData = userResult.rows[0];
+      console.log(userData);
+
+      // Buscar o login associado ao usuário na tabela tb_login
+      const loginQuery = 'SELECT * FROM "PeDeByteSchema".tb_login WHERE id_login = $1;';
+      const loginResult = await client.query(loginQuery, [userData.login_id]);
 
       if (loginResult.rows.length === 0) {
         throw new Error('Usuário ou senha inválidos');
       }
 
       const loginData = loginResult.rows[0];
-      console.log(loginResult.rows[0]);
 
       // Verificar a senha utilizando o bcrypt
       const isPasswordValid = bcrypt.compareSync(password, loginData.token); // Supondo que "token" é a senha criptografada
       if (!isPasswordValid) {
         throw new Error('Usuário ou senha inválidos');
       }
-
-      // Buscar o usuário associado
-      const userQuery = 'SELECT * FROM "PeDeByteSchema".tb_user WHERE login_id = $1;';
-      const userResult = await client.query(userQuery, [loginData.id_login]);
-
-      if (userResult.rows.length === 0) {
-        throw new Error('Usuário não encontrado');
-      }
-
-      const userData = userResult.rows[0];
 
       // Gerar um token JWT
       const token = jwt.sign({ id: userData.person_id }, JWT_SECRET, { expiresIn: '1h' });
